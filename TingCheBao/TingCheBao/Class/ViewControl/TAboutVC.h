312d187568ed04417a30aacf93b01f825684d63f
freeman22 <rendongzhao@163.com>
2014-04-28 23:02:21 +0800

    create sdk
    new class

---
+++ TingCheBao/TingCheBao/Class/ViewControl/TAboutVC.h
@@ -0,0 +1,13 @@
+//
+//  TAboutVC.h
+//  TingCheBao
+//
+//  Created by zhao on 14-4-28.
+//  Copyright (c) 2014年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAboutVC : UIViewController
+
+@end
---
+++ TingCheBao/TingCheBao/Class/ViewControl/THomePageVC.h
@@ -0,0 +1,13 @@
+//
+//  THomePageVC.h
+//  TingCheBao
+//
+//  Created by zhao on 14-4-28.
+//  Copyright (c) 2014年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THomePageVC : UIViewController
+
+@end
